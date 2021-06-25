378b68c175b6cab44983bfa0852ca418fbf812d3
Arturo Jamaica <me@arturojamaica.com>
2021-06-25 10:52:36 +0900

    Refactor transaction builder

---
+++ Solana/src/main/java/com/solana/core/TransactionBuilder.kt
@@ -0,0 +1,23 @@
+package com.solana.core
+
+class TransactionBuilder {
+    private val transaction: Transaction = Transaction()
+    fun addInstruction(transactionInstruction: TransactionInstruction?): TransactionBuilder {
+        transaction.addInstruction(transactionInstruction)
+        return this
+    }
+
+    fun setRecentBlockHash(recentBlockHash: String?): TransactionBuilder {
+        transaction.setRecentBlockHash(recentBlockHash)
+        return this
+    }
+
+    fun setSigners(signers: List<Account?>?): TransactionBuilder {
+        transaction.sign(signers)
+        return this
+    }
+
+    fun build(): Transaction {
+        return transaction
+    }
+}
