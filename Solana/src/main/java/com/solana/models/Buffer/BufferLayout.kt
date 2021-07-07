252d7036bdb4ef0e7f0f10f4d42dfa9325ad3f2c
Arturo Jamaica <me@arturojamaica.com>
2021-07-07 10:57:45 +0900

    Remove code

--- Solana/src/main/java/com/solana/models/Buffer/BufferLayout.kt
+++ Solana/src/main/java/com/solana/models/Buffer/BufferLayout.kt
@@ -3,35 +3,7 @@ package com.solana.models.Buffer
 import com.solana.models.RpcSendTransactionConfig
 import com.solana.vendor.borshj.Borsh
 import com.solana.vendor.borshj.BorshCodable
-import org.bitcoinj.core.Base58
 import java.util.*
-
-/*class Buffer<T: BorshCodable>{
-    val value: T?
-    constructor(borsh: Borsh, rawData: Any, clazz: Class<T>) {
-        if (rawData is String) {
-            value = rawData as T
-            return
-        }
-
-        val dataList = rawData as List<String>
-        if(dataList[0].isBlank() || dataList[0].length <= 0){
-            value = null
-            return
-        }
-
-        val decodedBytes = decodedData(dataList[0],dataList[1])
-        value = borsh.deserialize(decodedBytes, clazz)
-    }
-
-    private fun decodedData(serializedData: String, encoding: String): ByteArray {
-        return if (encoding == RpcSendTransactionConfig.Encoding.base64.toString()) {
-            Base64.getDecoder().decode(serializedData)
-        } else {
-            Base58.decode(serializedData)
-        }
-    }
-}*/
 
 data class Buffer2<T>(val value: T?){
     companion object {
