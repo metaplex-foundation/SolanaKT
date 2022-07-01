a740fa80da9ccd43bf28b414531548d178b2f0eb
Arturo Jamaica <me@arturojamaica.com>
2022-07-01 11:37:29 -0500

    Map result

--- solana/src/main/java/com/solana/api/sendTransaction.kt
+++ solana/src/main/java/com/solana/api/sendTransaction.kt
@@ -3,6 +3,7 @@ package com.solana.api
 import com.solana.core.Account
 import com.solana.core.Transaction
 import com.solana.models.RpcSendTransactionConfig
+import com.solana.vendor.flatMap
 import java.lang.RuntimeException
 import java.util.*
 
@@ -17,21 +18,23 @@ fun Api.sendTransaction(
             result.map { recentBlockHash ->
                 transaction.setRecentBlockHash(recentBlockHash)
                 transaction.sign(signers)
-                val serializedTransaction: ByteArray = transaction.serialize()
-                val base64Trx: String = Base64.getEncoder().encodeToString(serializedTransaction)
-                listOf(base64Trx, RpcSendTransactionConfig())
-            }.onSuccess {
-                router.request("sendTransaction", it, String::class.java, onComplete)
-            }.onFailure {
-                onComplete(Result.failure(RuntimeException(it)))
+                transaction.serialize().map {
+                    val base64Trx: String = Base64.getEncoder().encodeToString(it)
+                    listOf(base64Trx, RpcSendTransactionConfig())
+                }.onSuccess {
+                    router.request("sendTransaction", it, String::class.java, onComplete)
+                }.onFailure {
+                    onComplete(Result.failure(RuntimeException(it)))
+                }
             }
         }
     } else {
         transaction.setRecentBlockHash(recentBlockHash)
         transaction.sign(signers)
-        val serializedTransaction: ByteArray = transaction.serialize()
-        val base64Trx: String = Base64.getEncoder().encodeToString(serializedTransaction)
-        val params = listOf(base64Trx, RpcSendTransactionConfig())
-        router.request("sendTransaction", params, String::class.java, onComplete)
+        transaction.serialize().onSuccess {
+            val base64Trx: String = Base64.getEncoder().encodeToString(it)
+            val params = listOf(base64Trx, RpcSendTransactionConfig())
+            router.request("sendTransaction", params, String::class.java, onComplete)
+        }
     }
 }
