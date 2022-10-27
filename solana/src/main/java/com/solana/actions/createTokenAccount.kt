8bdf368ab67abe52994d886689035b4314470a3a
Arturo Jamaica <me@arturojamaica.com>
2022-10-27 12:03:21 -0500

    Rebase

--- solana/src/main/java/com/solana/actions/createTokenAccount.kt
+++ solana/src/main/java/com/solana/actions/createTokenAccount.kt
@@ -8,41 +8,49 @@ import com.solana.programs.SystemProgram
 import com.solana.programs.TokenProgram
 import com.solana.api.getMinimumBalanceForRentExemption
 import com.solana.core.Account
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 fun Action.createTokenAccount(
     account: Account,
     mintAddress: PublicKey,
     onComplete: ((Result<Pair<String, PublicKey>>) -> Unit)
 ) {
-    api.getMinimumBalanceForRentExemption(REQUIRED_ACCOUNT_SPACE){
-        it.onSuccess { balance ->
-            val transaction = Transaction()
-            val newAccount = HotAccount()
-            val createAccountInstruction = SystemProgram.createAccount(
-                fromPublicKey = account.publicKey,
-                newAccountPublickey = newAccount.publicKey,
-                lamports = balance,
-                REQUIRED_ACCOUNT_SPACE,
-                TokenProgram.PROGRAM_ID
-            )
-
-            transaction.add(createAccountInstruction)
-            val initializeAccountInstruction = TokenProgram.initializeAccount(
-                account = newAccount.publicKey,
-                mint = mintAddress,
-                owner = account.publicKey
-            )
-
-            transaction.add(initializeAccountInstruction)
-            this.serializeAndSendWithFee(transaction, listOf(account,newAccount), null){ result ->
-                result.onSuccess { transactionId ->
-                    onComplete(Result.success(Pair(transactionId, newAccount.publicKey)))
-                }.onFailure { error ->
-                    onComplete(Result.failure(error))
-                }
-            }
-        }.onFailure {
-            onComplete(Result.failure(it))
-        }
+    CoroutineScope(dispatcher).launch {
+        onComplete(createTokenAccount(account, mintAddress))
     }
+}
+
+suspend fun Action.createTokenAccount(
+    account: Account,
+    mintAddress: PublicKey,
+) : Result<Pair<String, PublicKey>> {
+
+    val balance = api.getMinimumBalanceForRentExemption(REQUIRED_ACCOUNT_SPACE).getOrElse {
+        return Result.failure(it)
+    }
+
+    val transaction = Transaction()
+    val newAccount = HotAccount()
+    val createAccountInstruction = SystemProgram.createAccount(
+        fromPublicKey = account.publicKey,
+        newAccountPublickey = newAccount.publicKey,
+        lamports = balance,
+        REQUIRED_ACCOUNT_SPACE,
+        TokenProgram.PROGRAM_ID
+    )
+
+    transaction.add(createAccountInstruction)
+    val initializeAccountInstruction = TokenProgram.initializeAccount(
+        account = newAccount.publicKey,
+        mint = mintAddress,
+        owner = account.publicKey
+    )
+
+    transaction.add(initializeAccountInstruction)
+    val transactionId = this.serializeAndSendWithFee(transaction, listOf(account,newAccount), null).getOrElse {
+        return Result.failure(it)
+    }
+
+    return Result.success(Pair(transactionId, newAccount.publicKey))
 }
